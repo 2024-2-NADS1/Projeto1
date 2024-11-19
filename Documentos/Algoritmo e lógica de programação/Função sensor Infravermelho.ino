0918c54a01a417c28c5df63cb8ae34097676aad9
Larissa de Almeida <94193684+larissaalmeidao@users.noreply.github.com>
2024-11-19 13:05:39 -0300

    Update Função sensor Infravermelho.ino

--- Documentos/Algoritmo e lógica de programação/Função sensor Infravermelho.ino
+++ Documentos/Algoritmo e lógica de programação/Função sensor Infravermelho.ino
@@ -1,37 +1,31 @@
 // VERIFICAÇÃO DO SENSOR INFRAVERMELHO
 void verificarSensorInfraV() {
-  int estadoSensor = analogRead(sensorInfra); // lê a entrada analógica do sensor. 
-  // O valor pode variar de 0 a 1023.
-  // 0 representa 0 volts e 1023 representa a tensão máxima 5 volts
-   float tensaoInfraV = (estadoSensor / 1023.0) * 5.0; // Converte para tensão
+  int estadoSensor = analogRead(sensorInfra); // Lê a entrada analógica do sensor
+  float tensaoInfraV = (estadoSensor / 1023.0) * 5.0; // Converte para tensão
 
-  int limiar = 500; // Defina um valor de limiar apropriado
-  unsigned long tempo = millis(); // tempo atual em milissegundos
+  unsigned long tempo = millis(); // Tempo atual em milissegundos
 
-// Verifica se a tensão do sensor é menor que o valor minimo aceitavel
-   if(tensaoInfraV < tensao){
-      Serial.println("Alerta : tensão baixa");
-    }else{
-      Serial.print("Tensão de alimentação: ");
-        Serial.println(tensaoInfraV); // Exibe a tensão medida
-    }
+  if(tensaoInfraV < tensaoMinima) {
+    Serial.println("Alerta: tensão baixa");
+    Blynk.notify("Alerta: tensão baixa");
+  } else {
+    Serial.print("Tensão de alimentação: ");
+    Serial.println(tensaoInfraV); // Exibe a tensão medida
+    Blynk.notify("Tensão de alimentação: " + String(tensaoInfraV));
+  }
 
-  // Identificar o momento em que um objeto foi detectado pelo sensor
-  if (estadoSensor < limiar && ultimoEstado >= limiar) {  
-    // Verifica se houve uma detecção recente (para evitar falsas leituras)
+  if (estadoSensor < threshold && ultimoEstado >= threshold) {  
     if (tempo - tempoDeteccao > 100) {
       Serial.println("Objeto detectado!");
-      Blynk.virtualWrite(V2, "Objeto detectado"); //Envia os dados para o Blynk
+      Blynk.notify("Objeto detectado!");
       tempoDeteccao = tempo;  // Atualiza o tempo da última detecção
     }
   } 
 
-  // Verifica se o sensor está a muito tempo sem detectar um objeto
   if (tempo - tempoDeteccao > 180000) { // Ajusta o tempo de intervalo em 3 minutos 
     Serial.println("Alerta: Nenhum objeto detectado por muito tempo");
-    Blynk.virtualWrite(V3, "Nenhum objeto detectado");
+    Blynk.notify("Alerta: Nenhum objeto detectado por muito tempo");   
   }
 
-  // Atualiza o último estado para comparação futura
-  ultimoEstado = estadoSensor;
+  ultimoEstado = estadoSensor; // Atualiza o último estado para comparação futura
 }
