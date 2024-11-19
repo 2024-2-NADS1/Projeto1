cf38ad7bb15eb27030471dfc37513cdbabec9d72
Larissa de Almeida <94193684+larissaalmeidao@users.noreply.github.com>
2024-11-19 13:04:19 -0300

    Create Função motor.ino

---
+++ Documentos/Algoritmo e lógica de programação/Função motor.ino
@@ -0,0 +1,9 @@
+// Função para parar os motores
+void pararMotores() {
+  digitalWrite(PINO_IN1, LOW); 
+  digitalWrite(PINO_IN 2, LOW);
+  digitalWrite(PINO_IN3, LOW);
+  digitalWrite(PINO_IN4, LOW);
+  digitalWrite(PINO_ENA, LOW);
+  digitalWrite(PINO_ENB, LOW);
+}
