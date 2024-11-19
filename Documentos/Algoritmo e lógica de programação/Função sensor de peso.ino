d978e0b57a14e94d7fa821c1093a312b8658e3ef
Larissa de Almeida <94193684+larissaalmeidao@users.noreply.github.com>
2024-11-19 13:05:01 -0300

    Update Função sensor de peso.ino

--- Documentos/Algoritmo e lógica de programação/Função sensor de peso.ino
+++ Documentos/Algoritmo e lógica de programação/Função sensor de peso.ino
@@ -1,32 +1,30 @@
-// VERIFIÇÃO DO SENSOR DE PESO
-void verificarSensorPeso(float min, float max, int maxConstantes){
+// VERIFICAÇÃO DO SENSOR DE PESO
+void verificarSensorPeso(float min, float max, int maxConstantes) {
+  if(scale.is_ready()) {
+    float peso = scale.get_units(); // Obtém a leitura do sensor
 
-// verifica se o sensor está inicializado
-  if(scale.is_ready()){
-    float peso = scale.get_units(); // obtem a leitura do sensor
-  
-    // verifica se o peso está dentro do ideal
-    if(peso < min || peso > max){
+    if(peso < min || peso > max) {
       Serial.println("Alerta, fora do limite");
-    }else{
-      Serial.println("peso"+peso); //correção na impressão do peso
-      Blynk.virtualWrite(V1, peso); //Envia os dados para o Blynk
+      Blynk.notify("Alerta, fora do limite");
+    } else {
+      Serial.println("Peso: " + String(peso)); // Correção na impressão do peso
+      Blynk.notify("Peso: " + String(peso));
     }
 
-    // verifica se a leitura é constante
     if (abs(peso - ultimaLeitura) < 0.01) {
-            leituraConsecutivas++; // incrementa +1 caso seja detectado uma repetição na leitura do peso 
+      leituraConsecutivas++; // Incrementa +1 caso seja detectado uma repetição na leitura do peso 
 
-            // verifica se o número de leitura consecutivas está passando do maximo permitido
-            if (leituraConsecutivas >= maxConstantes) {
-                Serial.println("Alerta: peso repetindo várias vezes");
-            }
-        } else {
-            leituraConsecutivas = 0; // Reseta se houver variação
-        }
+      if (leituraConsecutivas >= maxConstantes) {
+        Serial.println("Alerta: peso repetindo várias vezes");
+        Blynk.notify("Alerta: peso repetindo várias vezes");
+      }
+    } else {
+      leituraConsecutivas = 0; // Reseta se houver variação
+    }
 
-    ultimaLeitura = peso; // atualiza a última leitura
-  }else{
+    ultimaLeitura = peso; // Atualiza a última leitura
+  } else {
     Serial.println("Sensor não inicializado");
+    Blynk.notify("Sensor não inicializado");
   }
 }
