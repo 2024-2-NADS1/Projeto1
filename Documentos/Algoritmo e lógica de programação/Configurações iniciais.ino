3f23d7e6a6aa594e7d389c1632ce5c429eda5afd
Larissa de Almeida <94193684+larissaalmeidao@users.noreply.github.com>
2024-11-19 13:02:36 -0300

    Update Configurações iniciais.ino

--- Documentos/Algoritmo e lógica de programação/Configurações iniciais.ino
+++ Documentos/Algoritmo e lógica de programação/Configurações iniciais.ino
@@ -1,39 +1,32 @@
 void setup() {
+  Serial.begin(115200); // Começa a comunicação da ESP com a plataforma do Blynk
+  Blynk.begin(auth, ssid, pass); // Valida os dados passados da rede
 
-  Serial.begin(115200); // Comeca a comunicacao da esp com a plataforma do blynk
-  Blynk.begin(auth, ssid, pass); //valida os dados passados da rede
-
-  //configuração dos pinos como saida
+  // Configuração dos pinos como saída
   pinMode(PINO_ENA, OUTPUT); 
   pinMode(PINO_ENB, OUTPUT);
   pinMode(PINO_IN1, OUTPUT);
   pinMode(PINO_IN2, OUTPUT);
   pinMode(PINO_IN3, OUTPUT);
   pinMode(PINO_IN4, OUTPUT);
+  pinMode(led, OUTPUT);
+  
+  // Inicia o código com os motores parados
+  pararMotores();
 
-  //inicia o codigo com os motores parados
-  digitalWrite(PINO_IN1, LOW); 
-  digitalWrite(PINO_IN2, LOW);
-  digitalWrite(PINO_IN3, LOW);
-  digitalWrite(PINO_IN4, LOW);
-  digitalWrite(PINO_ENA, LOW);
-  digitalWrite(PINO_ENB, LOW);
-
-   #ifdef DEBUG_HX711
-
+  #ifdef DEBUG_HX711
   Serial.begin(115200); // Inicia a comunicação serial para depuração
   #endif
 
   scale.begin(pinData, pinClk); 
-  pinMode(sensorInfra, INPUT);
-  pinMode(led, OUTPUT);
 
-  if(!scale.is_ready()){
-    while(1);
-  }else {
+  if(!scale.is_ready()) {
+    while(1); // Espera até que o sensor esteja pronto
+  } else {
     Serial.println("Sensor de peso inicializado com sucesso.");
+    Blynk.notify("Sensor de peso inicializado com sucesso.");
   }
 
-  scale.set_scale(calibracao); // ajusta a escala. Converte em unidades de medidas reais
-  scale.tare(); // zera a balança
+  scale.set_scale(CALIBRACAO); // Ajusta a escala
+  scale.tare(); // Zera a balança
 }
